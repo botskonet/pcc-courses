f33e5fd058d5437341df48583662e544ce79524c
Michael Botsko <michaelbotsko@michael-botskos-imac.local>
2009-08-01 14:24:13 -0700

    more form cleanup

--- javascript-intro/examples/form-validate-2/form-values.js
+++ javascript-intro/examples/form-validate-2/form-values.js
@@ -1,16 +1,18 @@
 
 /**
- * 19 Lines
+ * 23 Lines
  * In pure javascript this finds all text input elements on the page and displays the values in the output div
  * 
  * Pros:
  *    - No external libraries needed, 
  *    - Uses plain javascript, no need to learn external library API
+ *    - Uses event listeners to keep javascript out of HTML
  *
  * Cons:
  *    - May not be cross-browser compatible, requires full testing
  *    - Does not wait until document has fully loaded, may add event listener too early
  *    - Longer, requires use of variables
+ *    - Need to use different event listening methods for different browsers
  *
  */
 function getValues(){
@@ -25,8 +27,12 @@ function getValues(){
 		
 			var elm = elements[i];
 		
-			if(elm.type == 'text'){
-				document.getElementById('output').innerHTML += elm.value+"<br>";
+			if(elm.type == 'text' || elm.type == 'textarea'){
+				document.getElementById('output').innerHTML += elm.name + ': ' + elm.value+"<br>";
+			}
+			
+			if(elm.type == 'select-one'){
+				document.getElementById('output').innerHTML += elm.name + ': ' + elm.options[elm.selectedIndex].value+"<br>";
 			}	
 		}
 	}
@@ -35,11 +41,11 @@ function getValues(){
 	
 }
 
+var getvals = document.getElementById('getvals');
 try{
-	window.addEventListener("click", getValues, false);
+	getvals.addEventListener("click", getValues, false);
 } catch(err){
-	var button = document.getElementById('but-pure-js');
-	button.attachEvent("click", getValues, false);
+	getvals.attachEvent("click", getValues, false);
 }
 
 
@@ -47,25 +53,27 @@ try{
 
 /**
  * 6 Lines
- * In jQuery this finds all text input elements on the page and displays the values in the output div
+ * In jQuery, this finds all text input elements on the page and displays the values in the output div
  * 
  * Pros:
  *    - Much less user-written code
+ *    - Use of CSS selectors, which web devs also need to know
  *    - Tested to work properly in all supported browsers
  *    - Graceful error handling
- *    = Does not need to iterate forms first
+ *    - No javascript inside of HTML required
+ *    - One event listener for all browsers
  *
  * Cons:
- *    - Requires external js library
+ *    - Requires external js library (jQuery)
  *    - Requires understanding of that library
  *
  */
 $(document).ready(function(){
 	
-	$('#but-jquery').click(function(){
+	$('#j-getvals').click(function(){
 		
 		$('#output').html('');
 		
-		$('input[type=text]').each( function(){ $('#output').append($(this).val()+"<br>") });
+		$('input[type!=submit], textarea, select').each( function(){ $('#output').append(this.name + ': ' + $(this).val()+"<br>") });
 	});
 });
