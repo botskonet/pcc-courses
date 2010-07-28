f1358ef1f43972a26bf4e24db921828968114eba
Michael Botsko <botsko@gmail.com>
2010-07-28 14:38:06 -0700

    beginning prep for class 2

--- php-intro/slide-examples/5-1.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include('anotherfile1.php');
-require('anotherfile2.php');
-
-?>
--- php-intro/slide-examples/5-2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(file_exists('anotherfile.php')){
-	include('anotherfile.php');
-} else {
-	print 'File not found!';
-}
-
-?>
