cc1c00b6c970b55fc4e3af9b2fb556ec27fea7e4
Michael Botsko <botsko@gmail.com>
2010-08-24 15:59:54 -0700

    minor webdev102 course tweaks, class 5 php tweaks

--- php-intro/class-5/dates.php
+++ php-intro/class-5/dates.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set('UTC');
+
 print time();
 print "<br>";
 
@@ -19,6 +21,25 @@ print date("Y/m/d", mktime(0, 0, 0, date('m')+12, date('d'), date('Y')));
 print "<br>";
 
 print date("Y/m/d", strtotime("+1 year"));
+print "<br>";
+
+
+// 5.2.0+
+$date = new DateTime('2011-01-01');
+print $date->format('Y-m-d H:i:s');
+print "<br>";
+
+$date = new DateTime('2011-01-01');
+$date->add(new DateInterval('P10D'));
+print $date->format('Y-m-d');
+print "<br>";
+
+$gm = gmdate('Y-m-d h:i:s a');
+print $gm;
+print "<br>";
+$date = new DateTime($gm);
+$date->setTimeZone(new DateTimeZone('America/Los_Angeles'));
+print $date->format('Y-m-d h:i:s a');
 print "<br>";
 
 ?>
